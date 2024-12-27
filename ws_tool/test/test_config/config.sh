9639601374438076905da946d3becf60805d33aa
Joel McCracken <mccracken.joel@gmail.com>
2024-12-27 14:34:14 -0500

    install nix, the big test, on CI

--- ws_tool/test/test_config/config.sh
+++ ws_tool/test/test_config/config.sh
@@ -1,7 +1,11 @@
 workstation_props_dotfiles_gitdir=".git-dotfiles"
 
+workstation_props__common=()
+workstation_props__common+=(prop_ws_current_settings_symlink)
+workstation_props__common+=(prop_ws_nix_daemon_installed)
+
 workstation_props_ci_ubuntu=()
-workstation_props_ci_ubuntu+=(prop_ws_current_settings_symlink)
+workstation_props_ci_ubuntu+=("${workstation_props__common[@]}")
 
 workstation_props_ci_macos=()
-workstation_props_ci_macos+=(prop_ws_current_settings_symlink)
+workstation_props_ci_macos+=("${workstation_props__common[@]}")
