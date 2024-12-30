2ede88e8b37ea2a7c111a902bf83184331fbeed1
Joel McCracken <mccracken.joel@gmail.com>
2024-12-30 17:42:13 -0500

    set up dotfiles in CI full setup

--- test/test_config/config.sh
+++ test/test_config/config.sh
@@ -3,9 +3,23 @@ workstation_props__common+=(prop_ws_current_settings_symlink)
 workstation_props__common+=(prop_ws_dotfiles_git_track)
 workstation_props__common+=(prop_ws_nix_daemon_installed)
 workstation_props__common+=(prop_ws_nix_global_config)
+workstation_props__common+=(prop_ws_df_dotfiles)
 
 workstation_props_ci_ubuntu=()
 workstation_props_ci_ubuntu+=("${workstation_props__common[@]}")
 
 workstation_props_ci_macos=()
 workstation_props_ci_macos+=("${workstation_props__common[@]}")
+
+
+workstation_props_dotfiles__common() {
+  dotfile --ln --dot bashrc
+}
+
+workstation_props_dotfiles_ci_ubuntu() {
+  workstation_props_dotfiles__common;
+}
+
+workstation_props_dotfiles_ci_macos() {
+  workstation_props_dotfiles__common;
+}
