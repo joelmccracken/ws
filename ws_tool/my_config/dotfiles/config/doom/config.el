b52e322a52123310528d1f6d87f972418d6b25c9
Joel McCracken <mccracken.joel@gmail.com>
2024-12-29 22:46:48 -0500

    merge stuff from workstation

--- ws_tool/my_config/dotfiles/config/doom/config.el
+++ ws_tool/my_config/dotfiles/config/doom/config.el
@@ -31,6 +31,10 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 
